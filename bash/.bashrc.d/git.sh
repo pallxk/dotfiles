e5796df2bfd20fb49c5a205f0cded610306db774
pallxk <github@pallxk.com>
2016-11-23 19:07:08 +0800

    git: Add `git cd` to cd to the top level of the repo

--- bash/.bashrc.d/git.sh
+++ bash/.bashrc.d/git.sh
@@ -17,6 +17,11 @@ git () {
 		if [[ $* != *--help* ]]; then
 			defaults=("--verbose")
 		fi
+
+	# git cd
+	elif [ "$command" = "cd" -a $# -eq 0 ]; then
+		cd "$(git rev-parse --show-toplevel)"
+		return
 	fi
 
 	# Execute the original command with defaults inserted
