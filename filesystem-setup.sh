20dc8b5d90911bb499e40c71efc84ee68d75d149
pallxk <github@pallxk.com>
2016-12-21 13:40:33 +0800

    xdg: Add xdg-user-dirs package and config

--- filesystem-setup.sh
+++ filesystem-setup.sh
@@ -3,3 +3,6 @@
 # Create folder structure beforehand, so that dotfiles in different repos can
 # be stowed together.
 [ -e ~/.bashrc.d/after ] || mkdir -p ~/.bashrc.d/after
+
+# Create XDG user dirs
+hash xdg-user-dirs-update 2> /dev/null && xdg-user-dirs-update
