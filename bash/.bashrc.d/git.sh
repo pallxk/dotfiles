ae1c9f9144c7ace8dfd43e76428469ebdd73a95a
pallxk <github@pallxk.com>
2017-01-06 17:19:15 +0800

    git: Use Git built for Windows on Cygwin when working with LFS

--- bash/.bashrc.d/git.sh
+++ bash/.bashrc.d/git.sh
@@ -22,6 +22,17 @@ git () {
 	elif [ "$command" = "cd" -a $# -eq 0 ]; then
 		cd "$(git rev-parse --show-toplevel)"
 		return
+
+	# git lfs
+	elif [ "$command" = "lfs" ]; then
+		# Use Git built for Windows on Cygwin when working with LFS
+		if [[ $(uname -s) = CYGWIN* ]]; then
+			local git="$(type -pa git | grep ^/cygdrive/ | head -1)"
+			if [ "$git" ]; then
+				"$git" "$command" "$@"
+				return
+			fi
+		fi
 	fi
 
 	# Execute the original command with defaults inserted
