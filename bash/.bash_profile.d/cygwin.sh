ad2ac5e769e59812276d104f7f89f6a5140aa84a
pallxk <github@pallxk.com>
2017-09-29 00:33:47 +0800

    bash: Fix "Can't locate Git.pm in @INC"

--- bash/.bash_profile.d/cygwin.sh
+++ bash/.bash_profile.d/cygwin.sh
@@ -5,3 +5,6 @@
 # CHERE_INVOKING is checked in /etc/profile,
 # which prevents auto cd to home directory when starting a new login shell.
 export CHERE_INVOKING=1
+
+# Fix "Can't locate Git.pm in @INC"
+export PERL5LIB=/usr/lib/perl5/vendor_perl/5.22
