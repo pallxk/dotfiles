7b99c615645970a8c6f006ba7bac80049204035c
pallxk <github@pallxk.com>
2017-10-12 09:15:32 +0800

    packages: Install Yarn via OS package manager

--- packages/install-rpm-packages.sh
+++ packages/install-rpm-packages.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 basedir=`dirname "$0"`
 
+# Yarn
+if [ ! -e /etc/yum.repos.d/yarn.repo ]; then
+	sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+fi
+
 for list in rpm linux; do
 	"$basedir/install-packages.sh" "yum install -y" "$basedir/$list.txt" true
 done
--- packages/install-ubuntu-packages.sh
+++ packages/install-ubuntu-packages.sh
@@ -13,6 +13,12 @@ if [ ! -e /etc/apt/sources.list.d/nodesource.list ]; then
 	fi
 fi
 
+# Yarn
+if [ ! -e /etc/apt/sources.list.d/yarn.list ]; then
+	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+	echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+	sudo apt-get update
+fi
 
 for list in linux ubuntu; do
 	"$basedir/install-packages.sh" "apt-get install -y" "$basedir/$list.txt" true
