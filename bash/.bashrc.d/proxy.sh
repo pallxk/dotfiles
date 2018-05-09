6cba3dca3f6831e76f40a5a6137d93d3aab1f93e
pallxk <github@pallxk.com>
2018-05-09 15:25:18 +0800

    bash: Export all_proxy and no more exports for UPPERCASE_PROXY

--- bash/.bashrc.d/proxy.sh
+++ bash/.bashrc.d/proxy.sh
@@ -1,58 +1,38 @@
 # Export various "protocol_proxy" variables,
-# using corresponding uppercase or lowercase variable values if available,
+# using one of existing *_proxy variable if available,
 # otherwise, using the default value: 'http://127.0.0.1:8080/'.
 function proxy_on() {
+	# all
+	if [[ -z $all_proxy ]]; then
+		if [[ $http_proxy ]]; then
+			all_proxy=$http_proxy
+		elif [[ $https_proxy ]]; then
+			all_proxy=$https_proxy
+		else
+			all_proxy=http://127.0.0.1:8080/
+		fi
+	fi
 	# http
-	if [[ -z $http_proxy && -z $HTTP_PROXY ]]; then
-		http_proxy=http://127.0.0.1:8080/
-		HTTP_PROXY=$http_proxy
-	elif [[ -z $http_proxy ]]; then
-		http_proxy=$HTTP_PROXY
-	elif [[ -z $HTTP_RPOXY ]]; then
-		HTTP_PROXY=$http_proxy
+	if [[ -z $http_proxy ]]; then
+		http_proxy=$all_proxy
 	fi
 	# https
-	if [[ -z $https_proxy && -z $HTTPS_PROXY ]]; then
-		https_proxy=$http_proxy
-		HTTPS_PROXY=$HTTP_PROXY
-	elif [[ -z $https_proxy ]]; then
-		https_proxy=$HTTPS_PROXY
-	elif [[ -z $HTTP_RPOXY ]]; then
-		HTTPS_PROXY=$https_proxy
+	if [[ -z $https_proxy ]]; then
+		https_proxy=$all_proxy
 	fi
 	# ftp
-	if [[ -z $ftp_proxy && -z $FTP_PROXY ]]; then
-		ftp_proxy=$http_proxy
-		FTP_PROXY=$HTTP_PROXY
-	elif [[ -z $ftp_proxy ]]; then
-		ftp_proxy=$FTP_PROXY
-	elif [[ -z $HTTP_RPOXY ]]; then
-		FTP_PROXY=$ftp_proxy
-	fi
-	# rsync
-	if [[ -z $rsync_proxy && -z $RSYNC_PROXY ]]; then
-		rsync_proxy=$http_proxy
-		RSYNC_PROXY=$HTTP_PROXY
-	elif [[ -z $rsync_proxy ]]; then
-		rsync_proxy=$RSYNC_PROXY
-	elif [[ -z $HTTP_RPOXY ]]; then
-		RSYNC_PROXY=$rsync_proxy
+	if [[ -z $ftp_proxy ]]; then
+		ftp_proxy=$all_proxy
 	fi
 	# exceptions
-	if [[ -z $no_proxy && -z $NO_PROXY ]]; then
+	if [[ -z $no_proxy ]]; then
 		no_proxy="localhost,127.0.0.1"
-		NO_PROXY=$no_proxy
-	elif [[ -z $no_proxy ]]; then
-		no_proxy=$NO_PROXY
-	elif [[ -z $NO_PROXY ]]; then
-		NO_PROXY=$no_proxy
 	fi
 
-	export http_proxy https_proxy ftp_proxy rsync_proxy no_proxy
-	export HTTP_PROXY HTTPS_PROXY FTP_PROXY RSYNC_PROXY NO_PROXY
+	export http_proxy https_proxy ftp_proxy all_proxy no_proxy
 }
 
 function proxy_off() {
-	export -n http_proxy https_proxy ftp_proxy rsync_proxy no_proxy
-	export -n HTTP_PROXY HTTPS_PROXY FTP_PROXY RSYNC_PROXY NO_PROXY
+	export -n http_proxy https_proxy ftp_proxy all_proxy no_proxy
+	export -n HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY NO_PROXY
 }
