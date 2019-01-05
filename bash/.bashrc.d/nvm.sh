a5fcf9f1d3f817889779acd36e11ef107c52f028
pallxk <github@pallxk.com>
2019-01-05 20:48:32 +0800

    bash: Load nvm from user dir

--- bash/.bashrc.d/nvm.sh
+++ bash/.bashrc.d/nvm.sh
@@ -1,2 +1,8 @@
+# Load NVM from user dir
+NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Load NVM from system dir
 # Load NVM into a shell session *as a function*
 [[ -f /usr/share/nvm/init-nvm.sh ]] && source /usr/share/nvm/init-nvm.sh
