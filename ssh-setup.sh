919333860f881901a246641bbec06431e233ea16
pallxk <github@pallxk.com>
2019-05-05 16:11:32 +0800

    ssh: Append or replace ssh config block

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
-# Because of the potential for abuse, other users shall have no permissions
+
+# SSH requires strict permissions on files and dirs
 umask 077
-# Create ~/.ssh direcotry if not existing
-test -d ~/.ssh || mkdir ~/.ssh
-# Import generic ssh config into ~/.ssh/config
-dirname=`dirname "$0"`
-cat "$dirname/ssh/.ssh/ssh_config" >> ~/.ssh/config
+
+# Create ~/.ssh and ~/.ssh/config if not existing
+mkdir -p ~/.ssh
+touch ~/.ssh/config
+
+dirname=$(realpath "$(dirname "$0")")
+cd "$dirname"
+identifier=$(basename "$dirname")
+
+# Append/Replace generic ssh config block to ~/.ssh/config
+if hash perl 2> /dev/null; then
+	perl -0777 -pi -e \
+		s%"(####### $identifier\n.+#######/$identifier\n|$)"%'`cat ssh/.ssh/ssh_config`'%se \
+		~/.ssh/config
+else
+	cat ssh/.ssh/ssh_config >> ~/.ssh/config
+fi
