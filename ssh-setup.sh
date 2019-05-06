a8e1019aad9c44002e8c05bebcfa81aea1ed9de2
pallxk <github@pallxk.com>
2019-05-06 13:45:33 +0800

    ssh: Fix ssh-setup.sh when appending to an existing ssh_config

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -10,12 +10,19 @@ touch ~/.ssh/config
 dirname=$(realpath "$(dirname "$0")")
 cd "$dirname"
 identifier=$(basename "$dirname")
+LF=$'\n'
+export BLOCK_BEGIN="$LF####### $identifier$LF$LF"
+export BLOCK_END="$LF#######/$identifier$LF$LF"
 
 # Append/Replace generic ssh config block to ~/.ssh/config
 if hash perl 2> /dev/null; then
 	perl -0777 -pi -e \
-		s%"(####### $identifier\n.+#######/$identifier\n|$)"%'`cat ssh/.ssh/ssh_config`'%se \
+		s%"($BLOCK_BEGIN.+$BLOCK_END|$)"%'$ENV{BLOCK_BEGIN}.`cat ssh/.ssh/ssh_config`.$ENV{BLOCK_END}'%es \
 		~/.ssh/config
 else
-	cat ssh/.ssh/ssh_config >> ~/.ssh/config
+	(
+		echo -n "$BLOCK_BEGIN"
+		cat ssh/.ssh/ssh_config
+		echo -n "$BLOCK_END"
+	) >> ~/.ssh/config
 fi
