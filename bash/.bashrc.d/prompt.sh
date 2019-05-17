4f99c19b95e2b719815fe64f223a6152ada45cb3
pallxk <github@pallxk.com>
2019-05-17 11:01:58 +0800

    bash: Use PS0 to reset color if available

--- bash/.bashrc.d/prompt.sh
+++ bash/.bashrc.d/prompt.sh
@@ -50,7 +50,13 @@ __prompt_command () {
 		     cmd='\[\033[00;32m\]'
 
 		# Remove color settings for command string before executing it
-		debugcmd='printf "\033[00m"'
+		if [ ${BASH_VERSINFO[0]} -gt 4 ] || [ ${BASH_VERSINFO[0]} -eq 4 ] && [ ${BASH_VERSINFO[1]} -ge 4 ]; then
+			# Bash >= 4.4 supports PS0
+			PS0="\033[00m"
+		else
+			# Fallback to use trap DEBUG
+			debugcmd='printf "\033[00m"'
+		fi
 
 		if [ "$SSH_TTY" ]; then
 			hostname='\[\033[01;32m\]@\h\[\033[00m\]'
