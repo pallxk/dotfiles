56cf4f0a6983790b021b6d72aa5b4da920138772
pallxk <github@pallxk.com>
2019-05-30 14:21:38 +0800

    bash: Fix unzip

--- bash/.bashrc.d/zip.sh
+++ bash/.bashrc.d/zip.sh
@@ -1,3 +1,5 @@
 # UnZip and ZipInfo environment options
-export UNZIP='-O CP936'
-export ZIPINFO='-O CP936'
+# Note: Not all install of UnZip and ZipInfo supports the following options.
+# And specify them in that case causes unzip/zipinfo to exit with code 10.
+#export UNZIP='-O CP936'
+#export ZIPINFO='-O CP936'
