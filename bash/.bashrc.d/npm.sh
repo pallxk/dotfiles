6958aefae6ed4c07926f6dabbe2daf455c50db38
pallxk <github@pallxk.com>
2020-10-09 14:18:24 +0800

    bash: Fix npm completion inside WSL

--- bash/.bashrc.d/npm.sh
+++ bash/.bashrc.d/npm.sh
@@ -4,6 +4,11 @@
 [[ $(uname -s) = CYGWIN* ]] && return
 
 # Check npm package is installed
-command -v npm > /dev/null || return
+npm_path=$(command -v npm)
+
+# No completion if we're inside WSL while npm is in Windows
+if [ -z "$npm_path" ] || [[ "$npm_path" = /mnt/* ]]; then
+	return
+fi
 
 . <(npm completion)
