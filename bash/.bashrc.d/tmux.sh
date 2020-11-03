eba7894e01ec47d023d79222487248147d8f5965
pallxk <github@pallxk.com>
2020-11-03 16:21:40 +0800

    bash: Attach to existing tmux session or create a new one on SSH session

--- bash/.bashrc.d/tmux.sh
+++ bash/.bashrc.d/tmux.sh
@@ -1,14 +1,7 @@
 ## Autorun tmux for SSH sessions
 if [[ $SSH_TTY ]]; then
 	if [[ -z $TMUX ]] && hash tmux 2> /dev/null; then
-		printf '\ntmux session list:\n'
-
-		if tmux ls; then
-			# If tmux sessions exist, just display the session list.
-			echo
-		else
-			# If no tmux sessions exist, start one.
-			tmux
-		fi
+		# Attach to existing session or create a new one.
+		tmux new-session -A
 	fi
 fi
