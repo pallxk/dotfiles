02e151c010118d59836ef428603ab0331170a232
pallxk <github@pallxk.com>
2021-01-26 09:08:59 +0800

    kubectl: Bash completion is automatically enabled now in Arch Linux

--- bash/.bashrc.d/kubectl.sh
+++
@@ -1,9 +0,0 @@
-# Enable tab completion for kubectl
-
-# Check bash-completion package is installed
-[ -f /etc/profile.d/bash_completion.sh ] || return
-
-# Check kubectl package is installed
-command -v kubectl > /dev/null || return
-
-. <(kubectl completion bash)
