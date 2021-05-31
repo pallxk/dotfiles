3b5e4882de56bde232951f5b6afb78f98a18ec2f
pallxk <github@pallxk.com>
2021-05-31 12:31:36 +0800

    bash: Auto run tmux on SSH only when SHLVL=1

--- bash/.bashrc.d/tmux.sh
+++ bash/.bashrc.d/tmux.sh
@@ -1,5 +1,8 @@
 ## Autorun tmux for SSH sessions
-if [[ $SSH_TTY ]]; then
+
+# SHLVL=1 ensures we don't auto run tmux in nested sessions, which also avoids
+# infinite loop when ssh into self.
+if [[ $SSH_TTY ]] && [[ $SHLVL -eq 1 ]]; then
 	if [[ -z $TMUX ]] && hash tmux 2> /dev/null; then
 		# Attach to existing session or create a new one.
 		tmux new-session -A
