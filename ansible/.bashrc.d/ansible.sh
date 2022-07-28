4f93f5627db8d4e7b3f614957aa0b50c4a560ada
pallxk <github@pallxk.com>
2022-07-28 21:31:49 +0800

    ansible: config stdout_callback and strategy

--- ansible/.bashrc.d/ansible.sh
+++ ansible/.bashrc.d/ansible.sh
@@ -1,2 +1,8 @@
 [ -f ~/bin/ansible-vault-password-helper ] || return
 export ANSIBLE_VAULT_PASSWORD_FILE=~/bin/ansible-vault-password-helper
+
+# https://docs.ansible.com/ansible/latest/reference_appendices/config.html#default-stdout-callback
+export ANSIBLE_STDOUT_CALLBACK=yaml
+
+# https://docs.ansible.com/ansible/latest/reference_appendices/config.html#default-strategy
+export ANSIBLE_STRATEGY=free
