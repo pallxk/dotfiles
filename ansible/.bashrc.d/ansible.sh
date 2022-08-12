ce42cb389fd0c64a0f6ac81b2512febefb93b921
pallxk <github@pallxk.com>
2022-08-12 15:43:13 +0800

    ansible: profile tasks

--- ansible/.bashrc.d/ansible.sh
+++ ansible/.bashrc.d/ansible.sh
@@ -1,6 +1,9 @@
 [ -f ~/bin/ansible-vault-password-helper ] || return
 export ANSIBLE_VAULT_PASSWORD_FILE=~/bin/ansible-vault-password-helper
 
+# https://docs.ansible.com/ansible/latest/reference_appendices/config.html#callbacks-enabled
+export ANSIBLE_CALLBACKS_ENABLED=profile_tasks
+
 # https://docs.ansible.com/ansible/latest/reference_appendices/config.html#default-stdout-callback
 export ANSIBLE_STDOUT_CALLBACK=yaml
 
