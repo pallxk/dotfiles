0705f9b95e7ff3272c3121b364379c359f1ecf19
pallxk <github@pallxk.com>
2022-08-29 13:55:20 +0800

    ansible: Enable pipelining

--- ansible/.bashrc.d/ansible.sh
+++ ansible/.bashrc.d/ansible.sh
@@ -4,6 +4,9 @@ export ANSIBLE_VAULT_PASSWORD_FILE=~/bin/ansible-vault-password-helper
 # https://docs.ansible.com/ansible/latest/reference_appendices/config.html#callbacks-enabled
 export ANSIBLE_CALLBACKS_ENABLED=profile_tasks
 
+# https://docs.ansible.com/ansible/latest/reference_appendices/config.html#ansible-pipelining
+export ANSIBLE_PIPELINING=True
+
 # https://docs.ansible.com/ansible/latest/reference_appendices/config.html#default-stdout-callback
 export ANSIBLE_STDOUT_CALLBACK=yaml
 
