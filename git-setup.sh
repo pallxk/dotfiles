b655e25163f666b2806e6f69b41484db2ab836d2
pallxk <github@pallxk.com>
2023-02-14 17:51:36 +0800

    git: fix ShellCheck

--- git-setup.sh
+++ git-setup.sh
@@ -7,13 +7,13 @@ touch ~/.gitconfig
 # Get current git version.
 # Output of `git --version` looks like this:
 #   git version 2.6.2
-current_version=`git --version | cut -d' ' -f3`
+current_version=$(git --version | cut -d' ' -f3)
 
 # Include $XDG_CONFIG_HOME/git/config manually if git version lower than 1.7.12
 # For git with higher version, this global config is included by default.
 target_version="1.7.12"
 
-if [ `printf "$target_version\n$current_version\n" | sort -V | head -1` \
+if [ "$(printf "%s\n%s\n" "$target_version" "$current_version" | sort -V | head -1)" \
      != "$target_version" ]; then
 	# current_version is lower than target_version
 	git config --global include.path .config/git/config
