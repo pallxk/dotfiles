4be4c7d6433a5a84ad92b7ae1592aef8631589b4
pallxk <github@pallxk.com>
2023-02-15 18:29:34 +0800

    bash: accept piped input for osc52

--- bash/.bashrc.d/osc52.sh
+++ bash/.bashrc.d/osc52.sh
@@ -1,6 +1,10 @@
 # https://jdhao.github.io/2021/01/05/nvim_copy_from_remote_via_osc52/
 osc52() {
-  content=$1
+  if [ -t 0 ]; then
+    content=$1
+  else
+    content=$(cat /dev/stdin)
+  fi
 
   printf "\e]52;c;$(printf "%s" "$content" | base64)\a"
 }
