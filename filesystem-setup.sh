a3f4b6fafac83635885780e3599f9980bc4293d2
hoomga <github@pallxk.com>
2023-04-08 21:08:06 +0800

    create ~/.config/otp when setting up filesystem

--- filesystem-setup.sh
+++ filesystem-setup.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Create folder structure beforehand, so that dotfiles in different repos can
 # be stowed together.
-dirs="$HOME/.bashrc.d/after $HOME/bin/after $HOME/.config $HOME/.cache/vim/backup $HOME/.cache/vim/swap $HOME/.cache/vim/undo"
+dirs=(
+	$HOME/.bashrc.d/after
+	$HOME/bin/after
+	$HOME/.config/otp
+	$HOME/.cache/vim/backup
+	$HOME/.cache/vim/swap
+	$HOME/.cache/vim/undo
+)
 
-for dir in $dirs; do
+for dir in "${dirs[@]}"; do
 	[ -e "$dir" ] || mkdir -p "$dir"
 done
 
