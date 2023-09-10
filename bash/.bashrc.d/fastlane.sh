851378898ddb9568ffa8bcc494914d46a8af9a25
mbp <i@lxk.sh>
2023-09-10 10:08:43 +0800

    bash: enable auto completion for fastlane

---
+++ bash/.bashrc.d/fastlane.sh
@@ -0,0 +1,5 @@
+# Enable tab completion for fastlane
+
+# Generate completion files with `fastlane enable_auto_complete`
+
+[ -f  ~/.fastlane/completions/completion.sh ] && . ~/.fastlane/completions/completion.sh
