44f16894f4c4892e2eecefd7d35f6c6c16c1b689
mbp <i@lxk.sh>
2024-05-11 10:24:57 +0800

    bash|backpack: add aliases for drsh and drbash

--- bash/.bashrc.d/docker.sh
+++ bash/.bashrc.d/docker.sh
@@ -1,4 +1,6 @@
 alias dr='docker run --rm -it -v "$PWD":/mnt:ro'
+alias drsh='docker run --rm -it -v "$PWD":/mnt:ro --entrypoint sh'
+alias drbash='docker run --rm -it -v "$PWD":/mnt:ro --entrypoint bash'
 
 dsh() {
     docker exec -it "$@" sh
