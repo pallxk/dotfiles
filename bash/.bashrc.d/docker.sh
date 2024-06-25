a71dee23f009744045c393db358e2f89961ee7c7
mbp <i@lxk.sh>
2024-06-25 09:03:27 +0800

    bash: add aliases drw, drwsh and drwbash for r/w mount

--- bash/.bashrc.d/docker.sh
+++ bash/.bashrc.d/docker.sh
@@ -1,7 +1,10 @@
 alias d=docker
 alias dr='docker run --rm --init -it -v "$PWD":/mnt:ro'
+alias drw='docker run --rm --init -it -v "$PWD":/mnt'
 alias drsh='docker run --rm -it -v "$PWD":/mnt:ro --entrypoint sh'
+alias drwsh='docker run --rm -it -v "$PWD":/mnt --entrypoint sh'
 alias drbash='docker run --rm -it -v "$PWD":/mnt:ro --entrypoint bash'
+alias drwbash='docker run --rm -it -v "$PWD":/mnt --entrypoint bash'
 
 dx() {
     docker exec -it "$@"
