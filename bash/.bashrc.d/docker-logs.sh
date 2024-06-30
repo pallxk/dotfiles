4abfd6cfe7dc2b4efdf16897ffa4c9654608c37c
mbp <i@lxk.sh>
2024-06-30 15:28:54 +0800

    bash/docker-logs: If no -- is specified, assume the last arg of this function is for logs

--- bash/.bashrc.d/docker-logs.sh
+++ bash/.bashrc.d/docker-logs.sh
@@ -10,6 +10,12 @@ sdlogs() {
     fi
   done
 
+  # If no -- is specified, assume the last arg of this function is for logs
+  if [ $# -eq 0 ]; then
+    set -- "${hosts[-1]}"
+    hosts=(${hosts[@]:0:${#hosts[@]}-1})
+  fi
+
   for host in "${hosts[@]}"; do
     command ssh "$host" docker logs "$@" &
   done
