e710e19c67fcf9617d5eca8397c3cc6ce22f8796
mbp <i@lxk.sh>
2024-06-30 15:40:12 +0800

    bash: add xssh.sh

---
+++ bash/.bashrc.d/xssh.sh
@@ -0,0 +1,25 @@
+xssh() {
+  hosts=()
+  while [ "$1" ]; do
+    if [ "$1" == -- ]; then
+      shift
+      break
+    else
+      hosts+=("$1")
+      shift
+    fi
+  done
+
+  # If no -- is specified, assume the last arg of this function is for ssh
+  if [ $# -eq 0 ]; then
+    set -- "${hosts[-1]}"
+    hosts=(${hosts[@]:0:${#hosts[@]}-1})
+  fi
+
+  for host in "${hosts[@]}"; do
+    command ssh "$host" "$@" &
+  done
+
+  # Be quiet on job exit
+  wait 2> /dev/null
+}
