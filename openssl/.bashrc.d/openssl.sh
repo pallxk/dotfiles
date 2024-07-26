ad1a03c5326f436f81f596adf71bfc4793381161
mbp <i@lxk.sh>
2024-07-26 09:29:07 +0800

    openssl: add showcerts and showcertstext functions

---
+++ openssl/.bashrc.d/openssl.sh
@@ -0,0 +1,10 @@
+# https://docs.openssl.org/1.1.1/man1/s_client/
+
+showcerts() {
+  openssl s_client -showcerts "$@" <<< Q
+}
+
+showcertstext() {
+  openssl s_client -showcerts "$@" <<< Q 2>/dev/null \
+    | openssl x509 -text -noout
+}
