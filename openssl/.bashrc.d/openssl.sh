3877804a06781c8d87056227af6458e7d1c60b7b
mbp <i@lxk.sh>
2024-07-31 15:23:59 +0800

    openssl: add bash function showcertspem()

--- openssl/.bashrc.d/openssl.sh
+++ openssl/.bashrc.d/openssl.sh
@@ -4,6 +4,11 @@ showcerts() {
   openssl s_client -showcerts "$@" <<< Q
 }
 
+showcertspem() {
+  openssl s_client -showcerts "$@" <<< Q 2>/dev/null \
+    | openssl x509
+}
+
 showcertstext() {
   openssl s_client -showcerts "$@" <<< Q 2>/dev/null \
     | openssl x509 -text -noout
