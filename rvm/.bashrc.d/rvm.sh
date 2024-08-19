f2406c0f85f72c0e5c29a345ffb26fcc86aa7e33
mbp <i@lxk.sh>
2024-08-19 11:00:06 +0800

    rvm: add rvm-preinstall()

--- rvm/.bashrc.d/rvm.sh
+++ rvm/.bashrc.d/rvm.sh
@@ -1,1 +1,9 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+# https://stackoverflow.com/questions/76922732/error-installing-ruby-with-rvm-rvm-make-j8
+rvm-preinstall() {
+  export CONFIGURE_ARGS=""
+  for ext in openssl readline libyaml zlib; do
+    CONFIGURE_ARGS="${CONFIGURE_ARGS} --with-$ext-dir=$(brew --prefix $ext)"
+  done
+}
