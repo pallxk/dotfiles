f7fcf05b56f54e034fe588b3eb9c124a3a583088
mbp <i@lxk.sh>
2024-08-19 17:16:04 +0800

    bash: set HISTFILE to a name different from default

--- bash/.bashrc.d/bash_history.sh
+++ bash/.bashrc.d/bash_history.sh
@@ -8,6 +8,9 @@ shopt -s histappend
 # Ignore lines matching the previous entry (ignoredups)
 # Ignore lines which begin with a space character (ignorespace)
 HISTCONTROL=ignoreboth
+# The name of the file in which command history is saved
+# Use a name different from default to avoid `bash --norc` removes old history.
+HISTFILE=~/.bash_history2
 # The maximum number of lines contained in the history file
 HISTFILESIZE=100000
 # The number of commands to remeber in the command history
