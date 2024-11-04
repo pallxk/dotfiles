cccc541232d2baded00e05754dc27e6c4e27c302
mbp <i@lxk.sh>
2024-11-04 15:47:23 +0800

    aliyun: alias ali=aliyun

--- bash/.bashrc.d/aliyun.sh
+++ bash/.bashrc.d/aliyun.sh
@@ -1,2 +1,5 @@
-hash aliyun 2> /dev/null \
-  && complete -C "$(which aliyun)" aliyun
+if hash aliyun 2> /dev/null; then
+  alias ali=aliyun
+  complete -C "$(which aliyun)" aliyun
+  complete -C "$(which aliyun)" ali
+fi
