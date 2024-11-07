b9aa7f3213bfc2bbf520c32d3593703968e5d8cf
mbp <i@lxk.sh>
2024-11-07 12:45:50 +0800

    fastlane: export FASTLANE_OPT_OUT_USAGE=1

--- bash/.bashrc.d/fastlane.sh
+++ bash/.bashrc.d/fastlane.sh
@@ -2,4 +2,5 @@
 
 # Generate completion files with `fastlane enable_auto_complete`
 
+export FASTLANE_OPT_OUT_USAGE=1
 [ -f  ~/.fastlane/completions/completion.sh ] && . ~/.fastlane/completions/completion.sh
