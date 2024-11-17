6c18c1a0f9ad028abce14ae33d241b777ac53128
mbp <i@lxk.sh>
2024-11-17 19:20:49 +0800

    bash: select bash or sh in docker automagically

--- bash/.bashrc.d/docker.sh
+++ bash/.bashrc.d/docker.sh
@@ -1,21 +1,36 @@
 alias d=docker
 alias dr='docker run --rm --init -it -v "$PWD":/mnt:ro'
 alias drw='docker run --rm --init -it -v "$PWD":/mnt'
-alias drsh='docker run --rm -it -v "$PWD":/mnt:ro --entrypoint sh'
-alias drwsh='docker run --rm -it -v "$PWD":/mnt --entrypoint sh'
-alias drbash='docker run --rm -it -v "$PWD":/mnt:ro --entrypoint bash'
-alias drwbash='docker run --rm -it -v "$PWD":/mnt --entrypoint bash'
 
 dx() {
     docker exec -it "$@"
 }
 
 dsh() {
-    docker exec -it "$@" sh
+    local now=$SECONDS
+    if ! docker exec -it "$@" bash; then
+        if [ $((SECONDS - now)) -le 2 ]; then
+            docker exec -it "$@" sh
+        fi
+    fi
 }
 
-dbash() {
-    docker exec -it "$@" bash
+drsh() {
+    local now=$SECONDS
+    if ! docker run --rm -it -v "$PWD":/mnt:ro --entrypoint bash "$@"; then
+        if [ $((SECONDS - now)) -le 2 ]; then
+            docker run --rm -it -v "$PWD":/mnt:ro --entrypoint sh "$@"
+        fi
+    fi
+}
+
+drwsh() {
+    local now=$SECONDS
+    if ! docker run --rm -it -v "$PWD":/mnt --entrypoint bash "$@"; then
+        if [ $((SECONDS - now)) -le 2 ]; then
+            docker run --rm -it -v "$PWD":/mnt --entrypoint sh "$@"
+        fi
+    fi
 }
 
 dgrep() {
