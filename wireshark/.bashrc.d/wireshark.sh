52db08a9e1ad88b6d6a216431a3ad931c325d0b4
mbp <i@lxk.sh>
2024-11-28 19:39:29 +0800

    wireshark: clean up on CTRL-C for sdshark()

--- wireshark/.bashrc.d/wireshark.sh
+++ wireshark/.bashrc.d/wireshark.sh
@@ -10,6 +10,7 @@ sdshark() {
   temp_fifo=$(mktemp -u)
   mkfifo -m 600 "$temp_fifo"
   wireshark -k -i "$temp_fifo" &
-  command ssh "$1" docker run --rm -i --network container:"$2" "${3:-nicolaka/netshoot}" dumpcap -w - "${@:4}" >"$temp_fifo"
+  command ssh "$1" docker run --rm --cidfile "/tmp/dshark-$2.cid" -i --network container:"$2" "${3:-nicolaka/netshoot}" dumpcap -w - "${@:4}" >"$temp_fifo"
+  command ssh "$1" docker rm -f "\$(<\"/tmp/dshark-$2.cid\")" \; rm -f "/tmp/dshark-$2.cid"
   rm -f "$temp_fifo"
 }
