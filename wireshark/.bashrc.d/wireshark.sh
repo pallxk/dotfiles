a92c63b86163389670b12610d1946c134b312f16
mbp <i@lxk.sh>
2024-12-01 10:29:05 +0800

    wireshark/shshark(): nullify stderr of `docker rm`
    If the dumpcap container exits by itself, the container will be gone.

--- wireshark/.bashrc.d/wireshark.sh
+++ wireshark/.bashrc.d/wireshark.sh
@@ -11,6 +11,6 @@ sdshark() {
   mkfifo -m 600 "$temp_fifo"
   wireshark -k -i "$temp_fifo" &
   command ssh "$1" docker run --rm --cidfile "/tmp/dshark-$2.cid" -i --network container:"$2" nicolaka/netshoot dumpcap -w - "${@:3}" >"$temp_fifo"
-  command ssh "$1" docker rm -f "\$(<\"/tmp/dshark-$2.cid\")" \; rm -f "/tmp/dshark-$2.cid"
+  command ssh "$1" docker rm -f "\$(<\"/tmp/dshark-$2.cid\")" \; rm -f "/tmp/dshark-$2.cid" 2>/dev/null
   rm -f "$temp_fifo"
 }
