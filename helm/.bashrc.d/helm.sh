078f4546ee6ce6c8440dbb31c0f5822dc5c44565
mbp <i@lxk.sh>
2026-08-14 09:58:13 +0800

    helm: add aliases

---
+++ helm/.bashrc.d/helm.sh
@@ -0,0 +1,7 @@
+if [ -f "/opt/homebrew/opt/helm@3/bin/helm" ]; then
+	alias helm3=/opt/homebrew/opt/helm@3/bin/helm
+fi
+
+if [ -f "/opt/homebrew/opt/helm@4/bin/helm" ]; then
+	alias helm4=/opt/homebrew/opt/helm@4/bin/helm
+fi
